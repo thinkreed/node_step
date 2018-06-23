f25caa35a07649b9be04746245e904fd0a00e0f0
thinkreed <thinkreed2017@outlook.com>
2018-06-23 08:37:16 +0800

    step 27 add some middleware

--- s27_website/server.js
+++ s27_website/server.js
@@ -1,6 +1,17 @@
 var express = require('express')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
+var morgan = require('morgan')
 
 var app = express()
+
+app.use(morgan('combined'))
+
+app.use(cookieParser())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 
 app.get('/', (req, res, next) => {
     res.end('this is express server')
