36f4a8ce483dfce7a63333c5d794ce3d51d7b808
thinkreed <thinkreed2017@outlook.com>
2018-06-27 07:41:42 +0800

    add site config

--- s28_novel_server/src/spiders/crawler.js
+++ s28_novel_server/src/spiders/crawler.js
@@ -1,3 +1,5 @@
+
+
 var cheerio = require('cheerio')
 var Request = require('request')
 var cache = []
@@ -5,14 +7,37 @@ var tasks = []
 
 export async function parse(entryUrl, siteParser) {
     tasks.push(entryUrl)
-    let bookUrls = await parseLink(entryUrl, siteParser)
+    let bookUrls = await parseLink(siteParser)
+    return bookUrls
 }
 
-async function parseLink(entryUrl, siteParser) {
-    while (tasks) {
-        Request(entryUrl, (err, res, body) => {
-
-        })
-    }
-    
+async function parseLink(siteParser) {
+    return new Promise((resolve, reject) => {
+        let results = new Set()
+        while (tasks) {
+            let nextUrl = tasks.pop()
+            Request(nextUrl, (err, res, body) => {
+                cache[nextUrl] = true
+                if (err) {
+                    continue
+                }
+                let parseResults = await siteParser.parsePage(body)
+                parseResults.site_list.forEach(element => {
+                    if (element in cache) {
+                        return
+                    } else {
+                        tasks.push(element)
+                    } 
+                })
+                parseResults.book_urls.forEach(element => {
+                    results.add(element)
+                })
+            })
+        }
+        if (results.length > 0) {
+            resolve(results)
+        } else {
+            reject(results)
+        }
+    })
 }
